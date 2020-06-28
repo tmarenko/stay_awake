f72a6d9875b648af983cba2995afb00f063fccf7
tmarenko <nkrayner@gmail.com>
2020-06-29 02:17:29 +0300

    restore last position fix

--- stay_awake.py
+++ stay_awake.py
@@ -8,7 +8,7 @@ from threading import Thread
 pyautogui.FAILSAFE = False
 MAX_NON_ACTIVE_TIME = 600  # in seconds
 LAST_ACTIVITY_TIME = time.time()
-LAST_MOUSE_POSITION = 0, 0
+LAST_MOUSE_POSITION = mouse.Controller().position
 
 
 def activity_listener(*args):
@@ -19,14 +19,15 @@ def activity_listener(*args):
 
 
 def make_activity_thread():
-    global LAST_ACTIVITY_TIME
+    global LAST_ACTIVITY_TIME, LAST_MOUSE_POSITION
     while True:
         current_time = time.time()
         if current_time - LAST_ACTIVITY_TIME > MAX_NON_ACTIVE_TIME:
             print("Reached max non-activity time.")
             LAST_ACTIVITY_TIME = current_time
+            restore_position = LAST_MOUSE_POSITION
             pyautogui.moveTo(0, 0)
-            pyautogui.moveTo(*LAST_MOUSE_POSITION)
+            pyautogui.moveTo(*restore_position)
             pyautogui.press("ctrl")
             print(f"Movement made at {datetime.now().time()}")
         time.sleep(1)
